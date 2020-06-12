bb7526a05a4c3515f881bb1cdc8c69a80ccddf1f
User <User@DESKTOP-S0PK0OE>
2020-06-11 21:12:25 -0700

    Add project files.

---
+++ BulkZipper/FormBulkZipper.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace BulkZipper
+{
+    partial class FormBulkZipper
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonZip = new System.Windows.Forms.Button();
+            this.textBoxFolder = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonOpenFolder = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxExtension = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelCount = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelProcessing = new System.Windows.Forms.Label();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonZip
+            // 
+            this.buttonZip.Location = new System.Drawing.Point(12, 127);
+            this.buttonZip.Name = "buttonZip";
+            this.buttonZip.Size = new System.Drawing.Size(63, 23);
+            this.buttonZip.TabIndex = 0;
+            this.buttonZip.Text = "Start";
+            this.buttonZip.UseVisualStyleBackColor = true;
+            this.buttonZip.Click += new System.EventHandler(this.buttonZip_Click);
+            // 
+            // textBoxFolder
+            // 
+            this.textBoxFolder.Location = new System.Drawing.Point(12, 29);
+            this.textBoxFolder.Name = "textBoxFolder";
+            this.textBoxFolder.Size = new System.Drawing.Size(217, 20);
+            this.textBoxFolder.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Folder containg files to be zipped";
+            // 
+            // buttonOpenFolder
+            // 
+            this.buttonOpenFolder.Location = new System.Drawing.Point(236, 27);
+            this.buttonOpenFolder.Name = "buttonOpenFolder";
+            this.buttonOpenFolder.Size = new System.Drawing.Size(42, 23);
+            this.buttonOpenFolder.TabIndex = 3;
+            this.buttonOpenFolder.Text = "...";
+            this.buttonOpenFolder.UseVisualStyleBackColor = true;
+            this.buttonOpenFolder.Click += new System.EventHandler(this.buttonOpenFolder_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Extension to be zipped";
+            // 
+            // textBoxExtension
+            // 
+            this.textBoxExtension.Location = new System.Drawing.Point(12, 90);
+            this.textBoxExtension.Name = "textBoxExtension";
+            this.textBoxExtension.Size = new System.Drawing.Size(217, 20);
+            this.textBoxExtension.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 196);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Count:";
+            // 
+            // labelCount
+            // 
+            this.labelCount.AutoSize = true;
+            this.labelCount.Location = new System.Drawing.Point(81, 196);
+            this.labelCount.Name = "labelCount";
+            this.labelCount.Size = new System.Drawing.Size(13, 13);
+            this.labelCount.TabIndex = 7;
+            this.labelCount.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 171);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Processing:";
+            // 
+            // labelProcessing
+            // 
+            this.labelProcessing.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelProcessing.AutoSize = true;
+            this.labelProcessing.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelProcessing.Location = new System.Drawing.Point(81, 171);
+            this.labelProcessing.MaximumSize = new System.Drawing.Size(180, 0);
+            this.labelProcessing.MinimumSize = new System.Drawing.Size(180, 0);
+            this.labelProcessing.Name = "labelProcessing";
+            this.labelProcessing.Size = new System.Drawing.Size(180, 13);
+            this.labelProcessing.TabIndex = 9;
+            this.labelProcessing.Text = "---";
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(98, 127);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(63, 23);
+            this.buttonStop.TabIndex = 10;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // FormBulkZipper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 234);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.labelProcessing);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.labelCount);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxExtension);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonOpenFolder);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxFolder);
+            this.Controls.Add(this.buttonZip);
+            this.Name = "FormBulkZipper";
+            this.Text = "Bulk Zipper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonZip;
+        private System.Windows.Forms.TextBox textBoxFolder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonOpenFolder;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxExtension;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelCount;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelProcessing;
+        private System.Windows.Forms.Button buttonStop;
+    }
+}
+
---
+++ BulkZipper/FormBulkZipper.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BulkZipper
+{
+    public partial class FormBulkZipper : Form
+    {
+        private bool stop = false;
+
+        public FormBulkZipper()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOpenFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.RootFolder = Environment.SpecialFolder.MyComputer;
+            dialog.SelectedPath = Environment.CurrentDirectory;
+            var result = dialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                textBoxFolder.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void buttonZip_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                labelCount.Text = "0";
+                labelProcessing.Text = "---";
+
+                if (textBoxFolder.Text == "")
+                {
+                    MessageBox.Show("Choose a destination folder", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                if (!Directory.Exists(textBoxFolder.Text))
+                {
+                    MessageBox.Show("Choose a valid folder", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                if (textBoxExtension.Text == "")
+                {
+                    MessageBox.Show("Type a extension", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                var files = Directory.GetFiles(textBoxFolder.Text, "*." + textBoxExtension.Text);
+
+                if (files == null || files.Length == 0)
+                {
+                    MessageBox.Show("No files found", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                int index = 1;
+
+                new Thread(() =>
+                {
+                    foreach (var file in files)
+                    {
+                        if (stop)
+                        {
+                            Thread.CurrentThread.Abort();
+                            stop = false;
+                        }
+
+                        var zippedName = textBoxFolder.Text + "\\" + GetFileNameNoExtension(file) + ".zip";
+                        var fileName = GetFileName(file);
+                        labelProcessing.Text = fileName;
+
+                        Ionic.Zip.ZipFile zip = new Ionic.Zip.ZipFile();
+                        zip.AddEntry(fileName, File.ReadAllBytes(file));
+
+                        if (File.Exists(zippedName))
+                        {
+                            File.Delete(zippedName);
+                        }
+
+                        zip.Save(zippedName);
+                        labelCount.Text = index.ToString();
+                        labelCount.Refresh();
+                        index++;
+                    }
+                }).Start();
+
+                labelProcessing.Text = "Done!";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public static string GetFileNameNoExtension(string file)
+        {
+            if (string.IsNullOrEmpty(file)) return "";
+            var fileName = file.Substring(file.LastIndexOf("\\") + 1);
+            var indexExtension = fileName.LastIndexOf(".");
+
+            if (indexExtension == -1)
+            {
+                return fileName;
+            }
+            else
+            {
+                return fileName.Substring(0, fileName.LastIndexOf("."));
+            }
+        }
+
+        public static string GetFileName(string file)
+        {
+            if (string.IsNullOrEmpty(file)) return "";
+            return file.Substring(file.LastIndexOf("\\") + 1);
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            stop = true;
+        }
+    }
+}
